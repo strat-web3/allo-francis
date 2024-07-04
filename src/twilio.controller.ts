53dadd8d78515b2eff7e984c78354d23bf2de325
julienbrg <julien@strat.cc>
2024-07-04 10:29:00 +0200

    simplify

--- src/twilio.controller.ts
+++ src/twilio.controller.ts
@@ -6,15 +6,7 @@ export class TwilioController {
   constructor(private readonly twilioService: TwilioService) {}
 
   @Post('incoming-call')
-  handleIncomingCall() {
-    const twiml = `
-      <Response>
-        <Dial callerId="${process.env.TWILIO_PHONE_NUMBER}">
-          <Number>${process.env.TARGET_PHONE_NUMBER}</Number>
-        </Dial>
-      </Response>
-    `;
-
-    return twiml;
+  async handleIncomingCall(): Promise<string> {
+    return this.twilioService.handleIncomingCall();
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/twilio.module';
+import { TwilioModule } from '../src/twilio.module';
+import { TwilioService } from '../src/twilio.service';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication;
+describe('TwilioController (e2e)', () => {
+  let app: any;
+  let twilioService: TwilioService;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [TwilioModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
+
+    twilioService = moduleFixture.get<TwilioService>(TwilioService);
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('/webhook/twilio/incoming-call (POST)', () => {
+    // Ensure correct route definition
+    it('should return a TwiML response', async () => {
+      const twimlResponse: string = `
+        <Response>
+          <Say>Hello! This is Francis. I promise I will help you someday.</Say>
+        </Response>
+      `;
+      jest
+        .spyOn(twilioService, 'handleIncomingCall' as any)
+        .mockResolvedValue(twimlResponse);
+
+      const response = await request(app.getHttpServer())
+        .post('/webhook/twilio/incoming-call') // Use the correct endpoint
+        .expect(201); // Update expectation to expect 201 Created
+
+      expect(response.text).toEqual(twimlResponse);
+    });
   });
 });
